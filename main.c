8d6873145ad3bd76fdbafed69a04a50427ccc08b
Germain Baudelaire <germainjones1@gmail.com>
2023-11-14 21:30:07 +0000

    Complete Parallel Implementation

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ void makeMatrixElementsZero(double **matrix, int size);
 
 void copyMatrix(double **original_matrix, double **destination_matrix, int size);
 
-void smoothMatrix(double **matrix, double **smoothed_matrix, int size, int number_of_threads);
+void smoothMatrix(double **matrix, double **smoothed_matrix, int size, int number_of_threads, double precision);
 
 double **mallocMatrix(int size);
 
@@ -31,29 +31,7 @@ void printMatrix(double **matrix, int size);
 
 void setMatrixElementsToZero(double **matrix, int size);
 
-void setMatrixEdgeElementsToOne(double **matrix, int size);
-
-double getAverageOfAdjacentValues(double **current_matrix, int row, int column);
-
-bool isEdgeCoordinate(int row, int column, int size);
-
-struct SharedThreadData {
-    int number_of_threads, matrix_size;
-    pthread_barrier_t barrier;
-    double **current_matrix, **next_matrix;
-};
-
-struct UniqueThreadData {
-    int thread_id;
-    struct SharedThreadData shared_thread_data;
-};
-
-void *thread_function(struct UniqueThreadData thread_data);
-
-
-void smoothElement(struct SharedThreadData shared_thread_data, int row, int column);
-
-pthread_mutex_t myMutex = PTHREAD_MUTEX_INITIALIZER;
+void setMatrixEdgeElementsTo(double **matrix, int size, double edge_value);
 
 //void* myThreadFunction(void* arg) {
 //    // Lock the mutex before accessing the critical section
@@ -95,10 +73,45 @@ pthread_mutex_t myMutex = PTHREAD_MUTEX_INITIALIZER;
 //    pthread_mutex_destroy(&myMutex);
 //
 //    return 0;
-//}
+double absoluteValue(double value);
+
+double getAverageOfAdjacentValues(double **current_matrix, int row, int column);
+
+bool isEdgeCoordinate(int row, int column, int size);
+
+struct SharedData {
+    int number_of_threads;
+    int matrix_size;
+    pthread_barrier_t barrier;
+    double **current_matrix;
+    double **next_matrix;
+    pthread_mutex_t delta_mutex;
+    double delta;
+    bool terminate;
+    double precision;
+};
+
+struct ThreadData {
+    int thread_id;
+};
+
+struct ThreadContext {
+    struct ThreadData thread_data;
+    struct SharedData *shared_data;
+};
+
+void *thread_function(void *thread_data);
+
+
+void smoothElement(struct ThreadContext *thread_context, int row, int column);
+
+struct SharedData getSharedData(int size, int number_of_threads, double precision);
+
+void updateDelta(struct ThreadContext *thread_context, double delta);
+
 
 int main() {
-    int size = 9;
+    int size = 12;
     int number_of_threads = 5;
 
     double **matrix = mallocMatrix(size);
@@ -106,11 +119,15 @@ int main() {
 
     // Set initial matrices values
     setMatrixElementsToZero(matrix, size);
-    setMatrixEdgeElementsToOne(matrix, size);
     setMatrixElementsToZero(smoothed_matrix, size);
+    setMatrixEdgeElementsTo(matrix, size, 1);
 
+    printf("Original Matrix:\n");
     printMatrix(matrix, size);
-    smoothMatrix(matrix, smoothed_matrix, size, number_of_threads);
+
+    smoothMatrix(matrix, smoothed_matrix, size, number_of_threads, 0.01);
+
+    printf("Smoothed Matrix:\n");
     printMatrix(smoothed_matrix, size);
 
     // Free memory of matrices
@@ -119,31 +136,43 @@ int main() {
     return 0;
 }
 
-void smoothMatrix(double **matrix, double **smoothed_matrix, int size, int number_of_threads) {
-    pthread_barrier_t barrier;
-    pthread_t threads[number_of_threads];
-    int thread_ids[number_of_threads];
-    struct UniqueThreadData thread_data[number_of_threads];
-    struct SharedThreadData shared_thread_data;
-    double **current_matrix = mallocMatrix(size);
-    double **nextMatrix = smoothed_matrix;
-    double **deltaMatrix = mallocMatrix(size);
-    copyMatrix(matrix, current_matrix, size);
-    // Initialize the barrier with the number of threads
-    pthread_barrier_init(&barrier, NULL, number_of_threads);
+struct SharedData getSharedData(int size, int number_of_threads, double precision) {
+    struct SharedData shared_data;
+    shared_data.matrix_size = size;
+    shared_data.number_of_threads = number_of_threads;
+    shared_data.precision = precision;
+    shared_data.delta = 0;
+    shared_data.delta_mutex = PTHREAD_MUTEX_INITIALIZER;
+    shared_data.terminate = false;
+    shared_data.current_matrix = mallocMatrix(size);
+    shared_data.next_matrix = mallocMatrix(size);
 
-    // Fill shared thread data
-    shared_thread_data.barrier = barrier;
-    shared_thread_data.number_of_threads = number_of_threads;
-    shared_thread_data.current_matrix = current_matrix;
-    shared_thread_data.matrix_size = size;
-    shared_thread_data.next_matrix = nextMatrix;
+    pthread_barrier_init(&shared_data.barrier, NULL, number_of_threads);
+    setMatrixElementsToZero(shared_data.current_matrix, shared_data.matrix_size);
+    setMatrixElementsToZero(shared_data.next_matrix, shared_data.matrix_size);
+
+    return shared_data;
+}
+
+void freeMemoryOfSharedData(struct SharedData *sharedData);
+
+void freeMemoryOfSharedData(struct SharedData *sharedData) {
+    freeMatrixMemory(sharedData->current_matrix, sharedData->matrix_size);
+    freeMatrixMemory(sharedData->next_matrix, sharedData->matrix_size);
+}
+
+void smoothMatrix(double **matrix, double **smoothed_matrix, int size, int number_of_threads, double precision) {
+    pthread_t threads[number_of_threads];
+    struct ThreadContext thread_contexts[number_of_threads];
+    struct SharedData shared_data = getSharedData(size, number_of_threads, precision);
+
+    copyMatrix(matrix, shared_data.current_matrix, size);
 
     // Create threads
     for (int i = 0; i < number_of_threads; ++i) {
-        thread_data[i].thread_id = i;
-        thread_data[i].shared_thread_data = shared_thread_data;
-        pthread_create(&threads[i], NULL, thread_function, &thread_data[i]);
+        thread_contexts[i].thread_data.thread_id = i;
+        thread_contexts[i].shared_data = &shared_data;
+        pthread_create(&threads[i], NULL, thread_function, (void *) &thread_contexts[i]);
     }
 
     // Wait for all threads to finish
@@ -151,9 +180,75 @@ void smoothMatrix(double **matrix, double **smoothed_matrix, int size, int numbe
         pthread_join(threads[i], NULL);
     }
 
-
     // Destroy the barrier
-    pthread_barrier_destroy(&barrier);
+    pthread_barrier_destroy(&shared_data.barrier);
+    copyMatrix(shared_data.current_matrix, smoothed_matrix, shared_data.matrix_size);
+    freeMemoryOfSharedData(&shared_data);
+}
+
+void *thread_function(void *arg) {
+    struct ThreadContext *thread_context = (struct ThreadContext *) arg;
+    int matrix_size = thread_context->shared_data->matrix_size;
+    int number_of_elements = matrix_size * matrix_size;
+    int thread_id = thread_context->thread_data.thread_id;
+    int number_of_threads = thread_context->shared_data->number_of_threads;
+    int row, column, flattened_index;
+
+    while (!thread_context->shared_data->terminate) {
+        for (int multiple = 0; multiple * number_of_threads + thread_id < number_of_elements; ++multiple) {
+            flattened_index = multiple * number_of_threads + thread_id;
+            row = flattened_index / matrix_size;
+            column = flattened_index % matrix_size;
+            smoothElement(thread_context, row, column);
+        }
+
+        pthread_barrier_wait(&thread_context->shared_data->barrier);
+
+
+        if (thread_id == 0) {
+            if (thread_context->shared_data->delta < thread_context->shared_data->precision) {
+                thread_context->shared_data->terminate = true;
+            }
+            thread_context->shared_data->delta = 0;
+            copyMatrix(thread_context->shared_data->next_matrix,
+                       thread_context->shared_data->current_matrix,
+                       matrix_size);
+//            printMatrix(thread_context->shared_data->current_matrix, thread_context->shared_data->matrix_size);
+        }
+        pthread_barrier_wait(&thread_context->shared_data->barrier);
+    }
+
+    return NULL;
+}
+
+void smoothElement(struct ThreadContext *thread_context, int row, int column) {
+    double average_adjacent_values, delta;
+    double **current_matrix, **next_matrix;
+    current_matrix = thread_context->shared_data->current_matrix;
+    next_matrix = thread_context->shared_data->next_matrix;
+
+    if (isEdgeCoordinate(row, column, thread_context->shared_data->matrix_size)) {
+        next_matrix[row][column] = current_matrix[row][column];
+        delta = 0;
+    } else {
+        average_adjacent_values = getAverageOfAdjacentValues(current_matrix, row, column);
+        next_matrix[row][column] = average_adjacent_values;
+        delta = absoluteValue(current_matrix[row][column] - average_adjacent_values);
+    }
+
+    updateDelta(thread_context, delta);
+}
+
+void updateDelta(struct ThreadContext *thread_context, double delta) {
+    // Lock the mutex before accessing the critical section
+    pthread_mutex_lock(&thread_context->shared_data->delta_mutex);
+
+    if (thread_context->shared_data->delta < delta) {
+        thread_context->shared_data->delta = delta;
+    }
+
+    // Unlock the mutex after finishing the critical section
+    pthread_mutex_unlock(&thread_context->shared_data->delta_mutex);
 }
 
 void copyMatrix(double **original_matrix, double **destination_matrix, int size) {
@@ -180,11 +275,11 @@ void makeMatrixElementsZero(double **matrix, int size) {
     }
 }
 
-void setMatrixEdgeElementsToOne(double **matrix, int size) {
+void setMatrixEdgeElementsTo(double **matrix, int size, double edge_value) {
     for (int row = 0; row < size; row++) {
         for (int column = 0; column < size; column++) {
             if (isEdgeCoordinate(row, column, size)) {
-                matrix[row][column] = 1;
+                matrix[row][column] = edge_value;
             } else {
                 matrix[row][column] = 0;
             }
@@ -238,39 +333,6 @@ void printMatrix(double **matrix, int size) {
     printf("\n");
 }
 
-void *thread_function(struct UniqueThreadData thread_data) {
-    // TODO: Make this use multiples to avoid pointless checks
-    int flattened_index;
-
-    printf("Thread %d is doing some work...\n", thread_data.thread_id);
-
-    for (int row = 0; row < thread_data.shared_thread_data.matrix_size; ++row) {
-        for (int column = 0; column < thread_data.shared_thread_data.matrix_size; ++column) {
-            flattened_index = row * thread_data.shared_thread_data.matrix_size + column;
-            if (flattened_index % thread_data.shared_thread_data.number_of_threads == thread_data.thread_id) {
-                smoothElement(thread_data.shared_thread_data, row, column);
-            }
-        }
-    }
-
-    // Wait at the barrier
-    pthread_barrier_wait(&thread_data.shared_thread_data.barrier);
-
-    printf("Thread %d continues its work after the barrier.\n", thread_data.thread_id);
-
-    return NULL;
-}
-
-void smoothElement(struct SharedThreadData shared_thread_data, int row, int column) {
-    double average_adjacent_values;
-
-    if (isEdgeCoordinate(row, column, shared_thread_data.matrix_size)) {
-        shared_thread_data.next_matrix[row][column] = shared_thread_data.current_matrix[row][column];
-    } else {
-        average_adjacent_values = getAverageOfAdjacentValues(shared_thread_data.current_matrix, row, column);
-        shared_thread_data.next_matrix[row][column] = average_adjacent_values;
-    }
-}
 
 double getAverageOfAdjacentValues(double **current_matrix, int row, int column) {
     return (current_matrix[row][column + 1] +
@@ -291,78 +353,11 @@ bool isEdgeCoordinate(int row, int column, int size) {
     }
 }
 
+double absoluteValue(double value) {
+    if (value < 0) {
+        return -value;
+    } else {
+        return value;
+    }
+}
 
-//void *thread_function(struct UniqueThreadData thread_data) {
-//    printf("Thread %d is doing some work...\n", thread_data.thread_id);
-//    printf("%i \n", thread_data.barrier);
-//    // Wait at the barrier
-//    pthread_barrier_wait(&thread_data.barrier);
-//
-//    printf("Thread %d continues its work after the barrier.\n", thread_data.thread_id);
-//
-//    return NULL;
-//}
-
-
-//int main() {
-//    pthread_t threads[NUM_THREADS];
-//    int thread_ids[NUM_THREADS];
-//
-//    // Initialize the barrier with the number of threads
-//    pthread_barrier_init(&barrier, NULL, NUM_THREADS);
-//
-//    // Create threads
-//    for (int i = 0; i < NUM_THREADS; ++i) {
-//        thread_ids[i] = i;
-//        pthread_create(&threads[i], NULL, thread_function, &thread_ids[i]);
-//    }
-//
-//    // Wait for all threads to finish
-//    for (int i = 0; i < NUM_THREADS; ++i) {
-//        pthread_join(threads[i], NULL);
-//    }
-//
-//    // Destroy the barrier
-//    pthread_barrier_destroy(&barrier);
-//
-//    return 0;
-//}
-//int main() {
-//    int rows = 5;
-//    int columns = 5;
-//    double **current_matrix = mallocMatrix(rows, columns);
-//    makeMatrixElementsZero(current_matrix, rows, columns);
-//}
-
-//struct number {
-//    int uniqueNumber;
-//};
-
-//void *showUniqueNumber(void *numberStruct);
-
-//int main()
-//{
-//    pthread_t threadID[NUM_THREADS];
-//    struct number numbers[NUM_THREADS];
-//
-//    for(int i = 0; i < NUM_THREADS; i++)
-//    {
-//        numbers[i].uniqueNumber = i;
-//        pthread_create(&threadID[i], NULL, showUniqueNumber, numbers + i);
-//    }
-//
-//    for(int i=0; i < NUM_THREADS; i++)
-//    {
-//        pthread_join(threadID[i], NULL);
-//    }
-//
-//    return 0;
-//}
-
-//void *showUniqueNumber(void *numberStruct) {
-//    struct number *threadPtrN = numberStruct;
-//
-//    printf("%i\n", threadPtrN->uniqueNumber);
-//
-//    return (void *) 0;
-//}
