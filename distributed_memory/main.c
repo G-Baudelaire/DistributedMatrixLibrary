38eb72e0430f01442b7e3cf10d8d0138137dba3f
Germain Baudelaire <germainjones1@gmail.com>
2024-01-04 19:31:33 +0000

    Scatterv and Gatherv working as intended.

--- distributed_memory/main.c
+++ distributed_memory/main.c
@@ -39,54 +39,35 @@ double **mallocMatrixRows(int size);
 
 void mallocMatrixColumns(double **matrix, int size);
 
-void rank_0_logic();
+void rank_0_sequential_logic();
 
-void compute_rows_to_smooth(int matrix_size, int *output_array,
-                            int num_workers);
+void compute_rows_to_smooth(int *output_array, int matrix_size,
+                            int num_processes);
 
 void compute_partition_start(int *output_array, int *rows_to_smooth,
-                             int num_workers);
+                             int num_processes);
 
 void compute_partition_end(int *output_array, int *rows_to_smooth,
-                           int num_workers);
-
-void compute_flattened_start(int *output_array, int *partition_start,
-                             int matrix_size, int num_workers);
+                           int num_processes);
 
 void compute_flattened_end(int *output_array, int *partition_end,
-                           int matrix_size, int num_workers);
+                           int matrix_size, int num_processes);
 
 void compute_counts(int *output_array, int *flattened_start, int *flattened_end,
-                    int num_workers);
+                    int num_processes);
 
 void flatten_matrix(double **input_matrix, double *output_flattend_array,
-                    const int matrix_size);
+                    int matrix_size);
 
-// int main(int argc, char *argv[]) {
-//     // Check if the correct number of command-line arguments is provided
-//     if (argc != 3) {
-//         fprintf(stderr, "Usage: %s -n <number>\n", argv[0]);
-//         return 1; // Return an error code
-//     }
-//
-//     // Check if the first argument is the flag "-n"
-//     if (strcmp(argv[1], "-n") != 0) {
-//         fprintf(stderr, "Usage: %s -n <number>\n", argv[0]);
-//         return 1; // Return an error code
-//     }
-//
-//     // Convert the second argument (number) to an integer
-//     int number = atoi(argv[2]);
-//
-//     // Use the integer in your program
-//     printf("The provided number is: %d\n", number);
-//
-//     return 0; // Return success
+void compute_gatherv_displacement(int *output_array, int *flattened_start,
+                                  int matrix_size, int num_processes);
 
-// }
+void compute_gatherv_counts(int *output_array, int *counts, int matrix_size,
+                            int num_processes);
+
 void printArrayInt(int *array, int array_len);
 
-void printArrayDouble(double *array, double array_len);
+void printArrayDouble(double *array, int array_len);
 
 void setup(int num_worker_processes, int matrix_size);
 
@@ -119,7 +100,7 @@ void printArrayInt(int *array, int array_len) {
     printf("]\n");
 }
 
-void printArrayDouble(double *array, double array_len) {
+void printArrayDouble(double *array, int array_len) {
     printf("[");
 
     for (int i = 0; i < array_len; ++i) {
@@ -129,72 +110,121 @@ void printArrayDouble(double *array, double array_len) {
     printf("]\n");
 }
 
+void compute_scatterv_displacement(int *output, int matrix_size,
+                                   int num_processes) {
+    int *rows_to_smooth = malloc(matrix_size * sizeof(int));
+    int *partition_start = malloc(matrix_size * sizeof(int));
+
+    compute_rows_to_smooth(rows_to_smooth, matrix_size, num_processes);
+    compute_partition_start(partition_start, rows_to_smooth, num_processes);
+    free(rows_to_smooth);
+
+    for (int i = 0; i < num_processes; ++i) {
+        output[i] = partition_start[i] * matrix_size;
+    }
+    free(partition_start);
+}
+
+void compute_scatterv_counts(int *output, int *scatterv_displacement,
+                             int matrix_size, int num_processes) {
+    int *rows_to_smooth = malloc(matrix_size * sizeof(int));
+    int *partition_end = malloc(matrix_size * sizeof(int));
+    int *flattened_end = malloc(matrix_size * sizeof(int));
+
+    compute_rows_to_smooth(rows_to_smooth, matrix_size, num_processes);
+    compute_partition_end(partition_end, rows_to_smooth, num_processes);
+    free(rows_to_smooth);
+
+    compute_flattened_end(flattened_end, partition_end, matrix_size,
+                          num_processes);
+    free(partition_end);
+
+    compute_counts(output, scatterv_displacement, flattened_end, num_processes);
+    free(flattened_end);
+}
+
 int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
-    const int SIZE = 3;
+    const int MATRIX_SIZE = 7;
     const double PRECISION = 2;
     int num_processes;
     MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
-    int num_workers = num_processes - 1;
 
+    bool terminate = true;
     int task_id;
     MPI_Comm_rank(MPI_COMM_WORLD, &task_id);
 
-    int rows_to_smooth[num_workers];
-    int partition_start[num_workers], partition_end[num_workers];
-    int flattened_start[num_workers], flattened_end[num_workers];
-    int counts[num_processes];
+    const int array_memory = num_processes * sizeof(int);
+    int *scatterv_displacement = malloc(array_memory);
+    int *scatterv_counts = malloc(array_memory);
+    int *gatherv_counts = malloc(array_memory);
+    int *gatherv_displacement = malloc(array_memory);
+    double *send_buffer = NULL;
+    double *recv_buffer = NULL;
 
-    compute_rows_to_smooth(SIZE, rows_to_smooth, num_workers);
-    compute_partition_start(partition_start, rows_to_smooth, num_workers);
-    compute_partition_end(partition_end, rows_to_smooth, num_workers);
-    compute_flattened_start(flattened_start, partition_start, SIZE,
-                            num_workers);
-    compute_flattened_end(flattened_end, partition_end, SIZE, num_workers);
-    compute_counts(counts, flattened_start, flattened_end, num_workers);
+    // Setup
+    compute_scatterv_displacement(scatterv_displacement, MATRIX_SIZE,
+                                  num_processes);
+    compute_scatterv_counts(scatterv_counts, scatterv_displacement, MATRIX_SIZE,
+                            num_processes);
+    compute_gatherv_counts(gatherv_counts, scatterv_counts, MATRIX_SIZE,
+                           num_processes);
+    compute_gatherv_displacement(gatherv_displacement, scatterv_displacement,
+                                 MATRIX_SIZE, num_processes);
 
-    int displacement[num_processes];
-    displacement[0] = 0;
-    for (int i = 1; i < num_processes; ++i) {
-        displacement[i] = flattened_start[i - 1];
-    }
-    double *send_buffer;
-    double *recv_buffer;
 
     if (task_id == 0) {
-        send_buffer = malloc(SIZE * SIZE * sizeof(double));
-        for (int i = 0; i < SIZE * SIZE; ++i) {
+        send_buffer = malloc(MATRIX_SIZE * MATRIX_SIZE * sizeof(double));
+        for (int i = 0; i < MATRIX_SIZE * MATRIX_SIZE; ++i) {
             send_buffer[i] = i;
         }
-    } else {
-        recv_buffer = malloc(counts[task_id] * sizeof(double));
     }
+    recv_buffer = malloc(scatterv_counts[task_id] * sizeof(double));
 
-    MPI_Scatterv(send_buffer, counts, displacement, MPI_DOUBLE,
-                 recv_buffer, counts[task_id], MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    if (task_id == 1) {
-        // printf("Process 1 has array\n");
-        // printArrayDouble(recv_buffer, counts[task_id]);
-    }
+    MPI_Scatterv(send_buffer, scatterv_counts, scatterv_displacement,MPI_DOUBLE,
+                 recv_buffer, scatterv_counts[task_id], MPI_DOUBLE, 0,
+                 MPI_COMM_WORLD);
 
-    bool terminate = false;
-    double delta;
-    while (task_id == 0 && !terminate) {
-    }
-
-    while (task_id != 0 && !terminate) {
-
-    }
-    // Teardown
     if (task_id == 0) {
-        free(send_buffer);
-    } else {
-        free(recv_buffer);
+        printf("Process 0 has array\n");
+        printArrayDouble(recv_buffer, scatterv_counts[task_id]);
     }
+
+    double delta;
+    // Smoothing loop
+    while (!terminate) {
+    }
+
+    // Gather relevant data from each process
+    MPI_Gatherv(&recv_buffer[MATRIX_SIZE], gatherv_counts[task_id],MPI_DOUBLE,
+                send_buffer, gatherv_counts, gatherv_displacement,MPI_DOUBLE, 0,
+                MPI_COMM_WORLD);
+
+    // Teardown
+    free(scatterv_displacement);
+    free(scatterv_counts);
+    free(gatherv_counts);
+    free(gatherv_displacement);
+    free(send_buffer);
+    free(recv_buffer);
 
     MPI_Finalize();
     return 0;
+}
+
+void compute_gatherv_displacement(int *output_array, int *flattened_start,
+                                  int matrix_size, int num_processes) {
+    for (int i = 0; i < num_processes; ++i) {
+        output_array[i] = flattened_start[i] + matrix_size;
+    }
+}
+
+void compute_gatherv_counts(int *output_array, int *counts, int matrix_size,
+                            int num_processes) {
+    for (int i = 0; i < num_processes; ++i) {
+        output_array[i] = counts[i] - 2 * matrix_size;
+    }
 }
 
 void flatten_matrix(double **input_matrix, double *output_flattend_array,
@@ -208,37 +238,13 @@ void flatten_matrix(double **input_matrix, double *output_flattend_array,
     }
 }
 
-// int main(int argc, char* argv[]) {
-//     MPI_Init(&argc, &argv);
-//
-//
-//     int world_rank, world_size;
-//
-//     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-//     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-//
-//     int send_data[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-//     int recv_data[4];
-//
-//     int sendcounts[4] = {2, 3, 3, 1};
-//     int displs[4] = {0, 2, 2, 9};
-//
-//     MPI_Scatterv(send_data, sendcounts, displs, MPI_INT, recv_data, 4, MPI_INT, 0, MPI_COMM_WORLD);
-//
-//     for (int i = 0; i < 4; i++) {
-//         printf("Process %d\tPosition:%i\tData %d\n", world_rank, i,recv_data[i]);
-//     }
-//
-//     MPI_Finalize();
-//     return 0;
-// }
 
-void compute_rows_to_smooth(int matrix_size, int *output_array,
-                            int num_workers) {
-    const int rows_per_thread = matrix_size / num_workers;
-    const int remainder = matrix_size % num_workers;
+void compute_rows_to_smooth(int *output_array, int matrix_size,
+                            int num_processes) {
+    const int rows_per_thread = matrix_size / num_processes;
+    const int remainder = matrix_size % num_processes;
 
-    for (int i = 0; i < num_workers; ++i) {
+    for (int i = 0; i < num_processes; ++i) {
         if (i < remainder) {
             output_array[i] = rows_per_thread + 1;
         } else {
@@ -248,60 +254,52 @@ void compute_rows_to_smooth(int matrix_size, int *output_array,
 }
 
 void compute_partition_start(int *output_array, int *rows_to_smooth,
-                             int num_workers) {
-    if (num_workers < 3) {
+                             int num_processes) {
+    if (num_processes < 3) {
         exit(NOT_ENOUGH_PROCESSES);
     }
 
     output_array[0] = 0;
-    for (int i = 1; i < num_workers; ++i) {
+    for (int i = 1; i < num_processes; ++i) {
         output_array[i] = output_array[i - 1] + rows_to_smooth[i - 1];
     }
 
-    for (int i = 1; i < num_workers; ++i) {
+    for (int i = 1; i < num_processes; ++i) {
         output_array[i] -= 1;
     }
 }
 
 void compute_partition_end(int *output_array, int *rows_to_smooth,
-                           int num_workers) {
-    if (num_workers < 3) {
+                           int num_processes) {
+    if (num_processes < 3) {
         exit(NOT_ENOUGH_PROCESSES);
     }
 
     output_array[0] = rows_to_smooth[0];
-    for (int i = 1; i < num_workers; ++i) {
+    for (int i = 1; i < num_processes; ++i) {
         output_array[i] = output_array[i - 1] + rows_to_smooth[i];
     }
 
     // Last row does have a row after it to check.
-    output_array[num_workers - 1] -= 1;
+    output_array[num_processes - 1] -= 1;
 }
 
-void compute_flattened_start(int *output_array, int *partition_start,
-                             int matrix_size, int num_workers) {
-    for (int i = 0; i < num_workers; ++i) {
-        output_array[i] = partition_start[i] * matrix_size;
-    }
-}
 
 void compute_flattened_end(int *output_array, int *partition_end,
-                           int matrix_size, int num_workers) {
-    for (int i = 0; i < num_workers; ++i) {
-        output_array[i] = matrix_size * partition_end[i] + matrix_size - 1;
+                           int matrix_size, int num_processes) {
+    for (int i = 0; i < num_processes; ++i) {
+        output_array[i] = matrix_size * (partition_end[i] + 1) - 1;
     }
 }
 
 void compute_counts(int *output_array, int *flattened_start, int *flattened_end,
-                    int num_workers) {
-    // counts[0] = 0 because the manager does not need data.
-    output_array[0] = 0;
-    for (int i = 0; i < num_workers; ++i) {
-        output_array[i + 1] = flattened_end[i] - flattened_start[i] + 1;
+                    int num_processes) {
+    for (int i = 0; i < num_processes; ++i) {
+        output_array[i] = flattened_end[i] - flattened_start[i] + 1;
     }
 }
 
-void rank_0_logic() {
+void rank_0_sequential_logic() {
     double **matrix, **output_matrix;
     int size = 4;
     double precision = 0.1;
